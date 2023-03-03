5142540add13448dcbcf3b786370b7782b47beea
Nagaraju7799 <108717946+Nagaraju7799@users.noreply.github.com>
2023-03-03 22:52:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,14 @@ provider "aws" {
     access_key = "AKIA4TEE32MAZ5BCTYVH"
     secret_key = "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"
 }
-resource "aws_instance" "server" {
-  ami           = "ami-0263e4deb427da90e"
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-07344315c134e1989"
-  security_groups = ["sg-08725003435509389"]
-  key_name = "nagaraju"
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
 
-  tags = {
-    Name = "terraform"
-  } 
+variable "user_names" {
+    description = "Iam Users"
+    type        = list(string)
+    default     = [ "user1","user2","user3" ]  
 }
 
