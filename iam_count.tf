b183b54d65ae8878305fde353bb102571993a5af
Nagaraju7799 <108717946+Nagaraju7799@users.noreply.github.com>
2023-03-03 17:19:26 +0530

    Update iam_count.tf

--- iam_count.tf
+++ iam_count.tf
@@ -2,15 +2,6 @@ provider "aws" {
     region = "us-east-1"
   
 }
-
-resource "aws_instance" "sample" {
-    ami  = "ami-0263e4deb427da90e"
-    instance_type = "t2.micro"
-
-    tags = {
-        name = "Terraform-EC2"
-    }
-}
 resource "aws_iam_user" "example" {
   count = length(var.user_names)
   name  = var.user_names[count.index]
